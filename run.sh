728709ae0e5b8c1aae4cb58126aad2dd51bbb18a
Roman Rihter <46124855+tdrkDev@users.noreply.github.com>
2022-08-24 00:01:00 +0300

    Update shorthand

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl 'https://git.yndxfuck.ru/yndxfuck-public/bootstrap/-/raw/master/main.sh' > /tmp/yfstrap.sh && chmod +x /tmp/yfstrap.sh && /tmp/yfstrap.sh; rm /tmp/yfstrap.sh
+
+curl 'https://git.yndxfuck.ru/yndxfuck-public/bootstrap/-/raw/master/main.sh' >/tmp/yf.sh 2>/dev/null
+if [ $? = 0 ]; then
+echo -e "\nДля работы требуется root\n"
+sudo bash /tmp/yf.sh <&2
+else
+echo -e "\nНе удалось скачать лаунчер\n"
+fi
